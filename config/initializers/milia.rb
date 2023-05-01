0021c2d752447609658ed28ffd7cf30aaad32082
Serhii Haleta <>
2023-05-02 01:06:01 +0200

    A lot of changes

---
+++ config/initializers/milia.rb
@@ -0,0 +1,1 @@
+#config.whitelist_tenant_params = [:plan]
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  as :user do
+    match 'user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
+  end
+
+  devise_for :users, :conntrollers => {
+    :registrations => 'milia/registrations',
+    :registrations => 'confirmations',
+    :sessions => 'milia/sessions',
+    :passwords => 'milia/passwords',
+  }
+
+  devise_for :models
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
